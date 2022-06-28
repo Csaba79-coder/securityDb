124ade5a7bd09adbe87241a4c22986aff7a48c02
Csaba79-coder <csabavadasz79@gmail.com>
2022-06-28 21:31:34 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/securitydb/SecurityDbApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.securitydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDbApplication.class, args);
+    }
+
+}
